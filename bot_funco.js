a0df5de7412bd1767cf606b637ec7c728de7eeb8
iqbal <asifmai@hotmail.com>
2020-02-19 22:44:54 +0500

    'update'

--- bot_funco.js
+++ bot_funco.js
@@ -1,32 +1,41 @@
 const fs = require('fs');
 const _ = require('underscore');
+const pLimit = require('p-limit');
 const Helper = require('./helpers');
 const {siteLink} = require('./config');
 let browser;
 let productsLinks = [];
+productsLinks = JSON.parse(fs.readFileSync('productsLinks.json', 'utf8'));
 
 module.exports.runBot = () => new Promise(async (resolve, reject) => {
   try {
     browser = await Helper.launchBrowser();
 
     // Fetch Products Links from site
-    await fetchProductsLinks();
-    console.log(`No of Products found on site: ${productsLinks.length}`);
-    productsLinks = _.uniq(productsLinks);
-    console.log(`No of Products found on site (after removing duplicates): ${productsLinks.length}`);
-    fs.writeFileSync('productsLinks.json', JSON.stringify(productsLinks));
+    // console.log(`Fetching Products Links from site...`);
+    // await fetchProductsLinks();
+    // console.log(`No of Products found on site: ${productsLinks.length}`);
+    // productsLinks = _.uniq(productsLinks);
+    // console.log(`No of Products found on site (after removing duplicates): ${productsLinks.length}`);
+    // fs.writeFileSync('productsLinks.json', JSON.stringify(productsLinks));
+
+    // Scrape Products Data
+    console.log(`Fetching Products Data...`);
+    await scrapeProducts();
 
     await browser.close();
     resolve(true);
   } catch (error) {
+    await browser.close();
     console.log(`runBot Error: ${error.message}`);
     reject(error);
   }
 })
 
 const fetchProductsLinks = () => new Promise(async (resolve, reject) => {
+  let page;
   try {
-    const page = await Helper.launchPage(browser, true);
+    page = await Helper.launchPage(browser, true);
     await page.goto(`${siteLink}/products?limit=192`, {timeout: 0, waitUntil: 'load'});
     await page.waitForSelector('.pagination > button:nth-last-child(2)');
     const noOfPages = parseInt(await Helper.getTxt('.pagination > button:nth-last-child(2)', page));
@@ -41,15 +50,72 @@ const fetchProductsLinks = () => new Promise(async (resolve, reject) => {
       let pageLinks = await Helper.getAttrMultiple('.products > .catalog-product a.item-figure-container', 'href', page);
       pageLinks = pageLinks.map(pl => siteLink + pl);
       productsLinks.push(...pageLinks);
-      console.log(productsLinks.length);
     }
     
     await page.close();
     resolve();
   } catch (error) {
+    await page.close();
     console.log(`fetchProductsLinks Error: ${error.message}`);
     reject(error);
   }
 })
+
+const scrapeProducts = () => new Promise(async (resolve, reject) => {
+  try {
+    const promises = [];
+    const limit = pLimit(1);
+
+    for (let i = 0; i < productsLinks.length; i++) {
+      promises.push(limit(() => scrapeProduct(i)));
+    }
+
+    await Promise.all(promises);
+
+    resolve(true);
+  } catch (error) {
+    console.log(`scrapeProducts Error: ${error.message}`);
+    reject(error);
+  }
+});
+
+const scrapeProduct = (prodIdx) => new Promise(async (resolve, reject) => {
+  let page;
+  try {
+    page = await Helper.launchPage(browser, true);
+    await page.goto(productsLinks[prodIdx], {timeout: 0, waitUntil: 'load'});
+    await page.waitForSelector('.product-info h1');
+    
+    const product = {url: productsLinks[prodIdx]};
+    product.title = await Helper.getTxt('.product-info h1', page);
+    product.releaseDate = await getCellVal('val', 'release date:', page);
+
+    console.log(product);
+    await page.close();
+    resolve(true);
+  } catch (error) {
+    await page.close();
+    console.log(`scrapeProduct [${productsLinks[prodIdx]}] Error: ${error.message}`);
+    reject(error);
+  }
+})
+
+const getCellVal = (valLink, label, page) => {
+  try {
+    let returnVal = '';
+    await page.waitForSelector('.product-details');
+    const props = await page.$$('.product-details > div');
+    for (let i = 0; i < props.length; i++) {
+      const propLabel = props[i].$eval('strong', elm => elm.innerText.trim().toLowerCase());
+      console.log(propLabel);
+    }
+
+    resolve(returnVal);
+  } catch (error) {
+    console.log(`getCellVal [${label}] Error: ${error.message}`);
+    reject(error);
+  }
+}
+
 
 this.runBot();
